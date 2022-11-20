858f2f1805c50a8411062e33050ed080f4f18543
zjf <839276847@qq.com>
2022-11-20 22:52:37 +0800

    re-system

--- src/main/java/entities/UserAccount.java
+++ src/main/java/entities/UserAccount.java
@@ -43,6 +43,16 @@ public class UserAccount {
      */
     public static final int MAX_REQUEST = 5;
 
+    public int reported = 0;
+
+    public int getReported() {
+        return reported;
+    }
+
+    public void setReported(int reported) {
+        this.reported = reported;
+    }
+
     /**
      * Create a user account.
      * @param email login username, also is the account holder's email address.
