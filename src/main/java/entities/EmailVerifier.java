fc2a8a14778efe44bef9789c97f9fb1983576d46
nathan-1022 <83197955+nathan-1022@users.noreply.github.com>
2022-12-01 11:43:09 +0800

    Add files via upload

---
+++ src/main/java/entities/EmailVerifier.java
@@ -0,0 +1,10 @@
+package entities;
+
+public interface EmailVerifier {
+
+    /**
+     *  Verify whether the email address email has a valid email format.
+     * @return return true if email is valid, and false otherwise.
+     */
+    boolean verify(String email);
+}
---
+++ src/main/java/entities/Hobbies.java
@@ -0,0 +1,13 @@
+package entities;
+
+public enum Hobbies {
+    READING("Reading"), TRAVELLING("Travelling"),
+    MUSIC("Music"), GAMES("Games"), SPORTS("Sports"),
+    FOOD("Food"), MOVIES_AND_TV("Movies and TV"), OTHERS("Others");
+
+    private final String value;
+    Hobbies(String value) {
+        this.value = value;
+    }
+
+}
