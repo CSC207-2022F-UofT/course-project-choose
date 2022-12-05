ec978abfec6257e6166310ba33598cf1445dde22
nathan-1022 <83197955+nathan-1022@users.noreply.github.com>
2022-12-06 02:02:21 +0800

    Add files via upload

--- src/main/java/data_access_storage/UserRepoManager.java
+++ src/main/java/data_access_storage/UserRepoManager.java
@@ -1,11 +1,66 @@
-package entities;
+package data_access_storage;
+import java.util.Map;
 
-public enum Gender {
-    FEMALE("Female"), MALE("Male");
+/**
+ * This class decides how to interact with the database.
+ * Layer:  use case
+ */
+public interface UserRepoManager {
 
-    private final String gender;
-    Gender(String gender) {
-        this.gender = gender;
-    }
-    public String getGender(){return this.gender; }
+    /**
+     * Check whether a user with an email address email already exists.
+     * @param email user's email address
+     * @return return true if a user already exists with the email address email, and false otherwise.
+     */
+    boolean existsByEmail(String email);
+
+    /**
+     * Delete user data.
+     * Call this method only if requestModel exists in our data.
+     * @param requestModel the user data to be deleted.
+     */
+    void delete(RequestModel requestModel);
+
+    /**
+     * Delete user data whose user email address is email.
+     * Call this method only if requestModel exists in our data.
+     * @param email the email address of the user data to be deleted.
+     */
+    void delete(String email);
+
+    /**
+     * Save user data
+     * @param requestModel the user data to be saved.
+     */
+    void save(RequestModel requestModel);
+
+    /**
+     * Update user data whose email address is email.
+     * @param email the email address of the user whose account will be updated.
+     * @param requestModel update user data to be requestModel
+     */
+    void update(String email, RequestModel requestModel);
+
+    /**
+     * Return user data whose email address is email
+     * Call this method only if a user exists with the email address email
+     * @param email the email address of a user
+     * @return return the user data whose email address is email.
+     */
+    RequestModel getUserAccount(String email);
+
+    /**
+     * Get all users' account information
+     * @return all users' account information
+     */
+    Map<String, RequestModel> getAllUserAccount();
+
+    /**
+     * Check if the input password matches input email's recorded password.
+     * @param email the email address that a user enters.
+     * @param password the password that a user enters
+     * @return true if the password matches and false otherwise.
+     */
+    boolean matchingPassword(String email, String password);
+
 }
