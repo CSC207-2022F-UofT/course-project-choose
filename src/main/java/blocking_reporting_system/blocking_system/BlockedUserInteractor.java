df0077bdb07133deab4c099d526ba21bea5e62a4
nathan-1022 <83197955+nathan-1022@users.noreply.github.com>
2022-12-08 21:28:47 -0500

    Update BlockedUserInteractor.java

--- src/main/java/blocking_reporting_system/blocking_system/BlockedUserInteractor.java
+++ src/main/java/blocking_reporting_system/blocking_system/BlockedUserInteractor.java
@@ -33,9 +33,7 @@ public class BlockedUserInteractor implements BlockedUserInputBoundary {
     }
 
     /**
-     * Take the email request from users and decide whether
-     * the requesters can see the requested email address.
-     * If so, return the requested email address, and empty string otherwise.
+     * block specifc user
      * @param blockedUserRequestModel a data model for the email request
      * @return a data model for showing the requested email or empty string.
      */
