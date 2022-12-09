fc73647c818c379c4b13f4e2394f211a63ad7e66
nathan-1022 <83197955+nathan-1022@users.noreply.github.com>
2022-12-08 21:29:35 -0500

    Update BlockedUserOutputBoundary.java

--- src/main/java/blocking_reporting_system/blocking_system/BlockedUserOutputBoundary.java
+++ src/main/java/blocking_reporting_system/blocking_system/BlockedUserOutputBoundary.java
@@ -1,15 +1,13 @@
 package blocking_reporting_system.blocking_system;
 
 /**
- * This class is the abstraction that allows MatcherUIInteractor to pass
- * the requested email address to MatcherUIPresenter without violating Dependency Rules
+ * the requested block user without violating Dependency Rules
  * of clean architecture.
  * Layer: Use Case Layer
  */
 public interface BlockedUserOutputBoundary {
 
     /**
-     * Prepare the returned requested email address for viewing
      * @param blockedStatus the requested email address
      * @return the requested email address that is prepared for viewing
      */
